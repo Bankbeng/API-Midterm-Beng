dc47f2178d1c52f7f2ba302f80019613e56767c5
Bankbeng <banksinghalath1@gmail.com>
2025-10-21 22:11:06 +0700

    Mid Term Exam

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-// Import the Express library
 const express = require('express');
+const apiRouter = require('./routes/apiRoute');
 
-// Create an Express application instance
 const app = express();
-
-// Define the port number the server will listen on
 const port = 3000;
 
-// Import the product router from the routes folder
-const productRouter = require('./routes/product.route');
+app.use("/", apiRouter);
 
-// Define a route for the root URL ('/')
-// When a GET request is made to '/', send 'Hi ladys' as the response
-app.get('/', (req, res) => {
-  res.send('Hi ladys');
-});
-
-// Use the product router for any routes starting with '/api/product'
-// This means requests to '/api/product' will be handled by productRouter
-app.use("/api/product", productRouter);
-
-// Start the server and listen on the specified port
-// When the server starts, log a message to the console
 app.listen(port, () => {
-  console.log(`http://localhost:${port} or http://localhost:${port}/api/product`);
+    console.log(`Lao-Top API running at http://localhost:${port}`);
 });
