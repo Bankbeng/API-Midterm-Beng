d5e60aeb30d768c0d09de5a9e872ef8452a91d05
Bankbeng <banksinghalath1@gmail.com>
2025-10-29 16:22:32 +0700

    Mid-Term Exam & Event Management API

--- server.js
+++ server.js
@@ -2,16 +2,25 @@
 
 const express = require('express');
 const app = express();
-const port = 3000; // Using a fixed port
+const port = 3000;
 
 app.use(express.json());
 
+const apiRouter = require('./routes/apiRoute');
 const venueRouter = require('./routes/venues');
 const attendeeRouter = require('./routes/attendees');
 const eventRouter = require('./routes/events');
 
+app.use('/', apiRouter);
 app.use('/venues', venueRouter);
 app.use('/attendees', attendeeRouter);
 app.use('/events', eventRouter);
 
-app.listen(port, () => console.log(`Server started on port http://localhost:${port}`));
+app.listen(port, () => console.log(`Server started on available endpoints:
+    http://localhost:${port}/
+    http://localhost:${port}/students
+    http://localhost:${port}/courses
+    http://localhost:${port}/venues
+    http://localhost:${port}/attendees
+    http://localhost:${port}/events
+`));
